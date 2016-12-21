1092a9aa5114432cbd27696e7f3b7b2c0f6767d5
Max <max.wassler@gmail.com>
2016-12-21 01:10:33 +0100

    reformat + comments

--- d2ip.js
+++ d2ip.js
@@ -2,29 +2,29 @@
 const dns = require('dns');
 const chalk = require('chalk');
 const domain = process.argv[2];
-let reg = new RegExp('[^a-z0-9-.]','i');
+let reg = new RegExp('[^a-z0-9-.]', 'i');
 
-if ( process.argv[2] === 'help') {
-	console.log(chalk.green('d2ip') + ' is designed to be used as fast as possible')
-	console.log('') //enter
-	console.log(chalk.blue('usage: node d2ip domain.com'));
-	process.exit()
+if (process.argv[2] === 'help') { //help command
+    console.log(chalk.green('d2ip') + ' is designed to be used as fast as possible')
+    console.log(chalk.blue('usage: node d2ip domain.com'));
+    console.log(chalk.yellow('more at github.com/skawm/domain2ip-cli'));
+    process.exit()
 }
-if ( reg.test(domain) ) {
-	//invalid characters found
-	console.log(chalk.bold.red('Please enter a valid domain name'));
-	}
-	else {
-	//invalid characters not found
-	dns.resolve4(domain, function (err, addresses) {
-		if (err) return console.log(chalk.blue(domain + ' has no IPv4'));
- 		console.log(chalk.blue("IPv4:"));
- 		console.log(addresses)
-	});
-	dns.resolve6(domain, function (err, addresses) {
-		if (err) return console.log(chalk.blue(domain + ' has no IPv6'));
- 		console.log(chalk.blue("IPv6:"));
- 		console.log(addresses);
-	});
- 	
-	}
+
+if (reg.test(domain)) {
+    //invalid characters found
+    console.log(chalk.bold.red('Please enter a valid domain name'));
+} else {
+    //invalid characters not found
+    dns.resolve4(domain, function(err, addresses) { //resolve IPv4
+        if (err) return console.log(chalk.blue(domain + ' has no IPv4'));
+        console.log(chalk.blue("IPv4:"));
+        console.log(addresses)
+    });
+    dns.resolve6(domain, function(err, addresses) { //resolve IPv6
+        if (err) return console.log(chalk.blue(domain + ' has no IPv6'));
+        console.log(chalk.blue("IPv6:"));
+        console.log(addresses);
+    });
+
+}
